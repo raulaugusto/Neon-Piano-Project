fae4679c2b94454a2207d9754d918e0ca2fc4b5e
Raul Augusto <raul.123augusto@gmail.com>
2021-11-15 20:32:51 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,211 @@
+var C = new Audio();
+C.src = "notes/Piano.mf.C3.mp3"
+
+var D = new Audio();
+D.src = "notes/Piano.mf.D3.mp3"
+
+var E = new Audio();
+E.src = "notes/Piano.mf.E3.mp3"
+
+var F = new Audio();
+F.src = "notes/Piano.mf.F3.mp3"
+
+var G = new Audio();
+G.src = "notes/Piano.mf.G3.mp3"
+
+var A = new Audio();
+A.src = "notes/Piano.mf.A3.mp3"
+
+var B = new Audio();
+B.src = "notes/Piano.mf.B3.mp3"
+
+var C1 = new Audio();
+C1.src = "notes/Piano.mf.C4.mp3"
+
+var D1 = new Audio();
+D1.src = "notes/Piano.mf.D4.mp3"
+
+var E1 = new Audio();
+E1.src = "notes/Piano.mf.E4.mp3" 
+
+var F1 = new Audio();
+F1.src = "notes/Piano.mf.F4.mp3"
+
+var G1 = new Audio();
+G1.src = "notes/Piano.mf.G4.mp3"
+
+var A1 = new Audio();
+A1.src = "notes/Piano.mf.A4.mp3"
+
+var B1 = new Audio();
+B1.src = "notes/Piano.mf.B4.mp3"
+
+var C2 = new Audio();
+C2.src = "notes/Piano.mf.C5.mp3"
+
+var D2 = new Audio();
+D2.src = "notes/Piano.mf.D5.mp3"
+
+var E2 = new Audio();
+E2.src = "notes/Piano.mf.E5.mp3"
+
+var F2 = new Audio();
+F2.src = "notes/Piano.mf.F5.mp3"
+
+var G2 = new Audio();
+G2.src = "notes/Piano.mf.G5.mp3"
+
+var A2 = new Audio();
+A2.src = "notes/Piano.mf.A5.mp3"
+
+var B2 = new Audio();
+B2.src = "notes/Piano.mf.B5.mp3"
+
+function stoper(){
+    C.pause();
+    C.currentTime = 0;
+
+    D.pause();
+    D.currentTime = 0;
+
+    E.pause();
+    E.currentTime = 0;
+
+    F.pause();
+    F.currentTime = 0;
+
+    G.pause();
+    G.currentTime = 0;
+
+    A.pause();
+    A.currentTime = 0;
+
+    B.pause();
+    B.currentTime = 0;
+
+    C1.pause();
+    C1.currentTime = 0
+
+    D1.pause();
+    D1.currentTime = 0;
+
+    E1.pause();
+    E1.currentTime = 0;
+
+    F1.pause();
+    F1.currentTime = 0;
+
+    G1.pause();
+    G1.currentTime = 0;
+
+    A1.pause();
+    A1.currentTime = 0;
+
+    B1.pause();
+    B1.currentTime = 0;
+
+    C2.pause();
+    C2.currentTime = 0;
+
+    D2.pause();
+    D2.currentTime = 0;
+
+    E2.pause();
+    E2.currentTime = 0;
+
+    F2.pause();
+    F2.currentTime = 0;
+
+    G2.pause();
+    G2.currentTime = 0;
+
+    A2.pause();
+    A2.currentTime = 0;
+
+    B2.pause();
+    B2.currentTime = 0;
+}
+
+function playc(){
+    C.play();
+}
+
+function playd(){
+    D.play();
+}
+
+function playe(){
+    E.play();
+}
+
+function playf(){
+    F.play();
+}
+
+function playg(){
+    G.play();
+}
+
+function playa(){
+    A.play();
+}
+
+function playb(){
+    B.play();
+}
+
+function playc1(){
+    C1.play();
+}
+
+function playd1(){
+    D1.play();
+}
+
+function playe1(){
+    E1.play();
+}
+
+function playf1(){
+    F1.play();
+}
+
+function playg1(){
+    G1.play();
+}
+
+function playa1(){
+    A1.play();
+}
+
+function playb1(){
+    B1.play();
+}
+
+function playc2(){
+    C2.play();
+}
+
+function playd2(){
+    D2.play();
+}
+
+function playe2(){
+    E2.play();
+}
+
+function playf2(){
+    F2.play();
+}
+
+function playg2(){
+    G2.play();
+}
+
+function playa2(){
+    A2.play();
+}
+
+function playb2(){
+    B2.play();
+}
